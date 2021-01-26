8b1741da9ed0b09e8631e6c23d1d3de6de3a034d
Livio Daljai <livio.daljani@gmail.com>
2021-01-26 13:27:22 -0500

    initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    //hi
+    public static void main(String[] args) {
+
+        System.out.println("hi");
+    }
+}
---
+++ src/test/java/helloTest.java
@@ -0,0 +1,6 @@
+public class helloTest {
+    //hi
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
