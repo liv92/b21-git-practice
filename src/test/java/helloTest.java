1fd6019bf9f7880b4edc9d1cd1fd880d44e3daf3
Livio Daljai <livio.daljani@gmail.com>
2021-01-26 13:43:50 -0500

    tendies

--- src/test/java/helloTest.java
+++ src/test/java/helloTest.java
@@ -3,5 +3,6 @@ public class helloTest {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println(" baby steps ");
+        System.out.println("get money");
     }
 }
